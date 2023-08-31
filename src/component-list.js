d11ebe01d11b43e093661c27023ab452221b4bbe
unknown <bhernandezg013@gmail.com>
2023-08-30 21:05:27 -0600

    version Final, Estilo CSS agregado

--- src/component-list.js
+++ src/component-list.js
@@ -1,7 +1,42 @@
-import { LitElement, html } from "lit";
+import { LitElement, html, css } from "lit";
 
 
 class ComponentList extends LitElement {
+
+    static styles = css`
+        .table-wrapper {
+            width: 100%;
+            overflow-x: auto;
+        }
+        .table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        .table th {
+            background-color: #333;
+            color: white;
+            padding: 10px;
+            text-align: left;
+        }
+
+        .table td, .table th {
+            padding: 8px;
+        }
+        
+        .td-alive-true {
+            border: 1px solid #7CFC00;
+            padding: 10px;
+        }
+        .td-alive-false {
+            border: 1px solid #FF0000;
+            padding: 10px;
+        }
+
+        .table tr:hover {
+            background-color: rgb(102,102,102);
+        }
+    `
 
     static properties = {
         arrayPersonajes: { type: Array },
@@ -16,32 +51,53 @@ class ComponentList extends LitElement {
     render() {
         return html`
         
-            <table>
+        <div class="table-wrapper">
+            <table class="table">
                 <thead>
                   <tr>
                     <th>ID</th>
                     <th>Imagen</th>
                     <th>Nombre</th>
                     <th>Especie</th>
+                    <th>Genero</th>
                     <th>House</th>
+                    <th>Fecha Nacimiento</th>
+                    <th>Año Nacimiento</th>
+                    <th>Wizard</th>
+                    <th>Ancestry</th>
+                    <th>Color Ojos</th>
+                    <th>Color Cabello</th>
                     <th>Patronus</th>
+                    <th>Estudiante</th>
+                    <th>Con vida</th>
+                    <th>Actor</th>
                   </tr>
                 </thead>
                 <tbody>
                     ${this.arrayPersonajes.map((personaje, index) => html`
-                        <tr>
+                        <tr class=${personaje.alive ? 'td-alive-true' : 'td-alive-false'} >
                             <td>${index + 1}</td>
                             <td><img src=${personaje.image ? personaje.image : 'https://th.bing.com/th/id/OIP.vvmpWt0qBu3LeBgZuUfmGAHaFt?pid=ImgDet&rs=1'} style="width: 50px;"></td>
                             <td>${personaje.name}</td>
                             <td>${personaje.species}</td>
+                            <td>${personaje.gender}</td>
                             <td>${personaje.house}</td>
+                            <td>${personaje.dateOfBirth}</td>
+                            <td>${personaje.yearOfBirth}</td>
+                            <td>${personaje.wizard}</td>
+                            <td>${personaje.ancestry}</td>
+                            <td>${personaje.eyeColour}</td>
+                            <td>${personaje.hairColour}</td>
                             <td>${personaje.patronus}</td>
+                            <td>${personaje.hogwartsStudent}</td>
+                            <td>${personaje.alive}</td>
+                            <td>${personaje.actor}</td>
                         </tr>
                     `)}
 
                 </tbody>
             </table>
-        
+        </div>
         `;
     }
 
--- src/ejercicio.js
+++ src/ejercicio.js
@@ -1,13 +1,59 @@
-import { LitElement, html } from "lit";
+import { LitElement, html, css } from "lit";
 import "./component-list"
 
 class Ejercicio extends LitElement {
+
+    static styles = css`
+        .section {
+            padding: 30px 0px 30px 0px;
+        }
+        .input-container {
+            height: 50px;
+            position: relative;
+            width: 100%;
+            margin-top: 30px;
+            margin-bottom: 30px;
+        }
+
+        .input {
+            background-color: #303245;
+            border-radius: 12px;
+            border: 0;
+            box-sizing: border-box;
+            color: #eee;
+            font-size: 18px;
+            height: 100%;
+            outline: 0;
+            padding: 4px 20px 0;
+            width: 100%;
+        }
+
+        .select-container {
+            width: 200px;
+        }
+          
+        .select-style {
+            background-color: #f2f2f2;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            padding: 8px;
+            width: 100%;
+            font-size: 14px;
+            color: #333;
+            cursor: pointer;
+        }
+        option{
+            cursor: pointer;
+        }
+    `;
 
     static properties = {
         arrayPersonajes: { tpe: Array },
         arrayFiltrados: { tpe: Array },
         arraySelect: { type: Array },
-        arraySelectFilter: { type: Array }
+        arraySelectFilter: { type: Array },
+        value: { type: String },
+        valueInput: { type: String }
     }
 
     constructor() {
@@ -28,14 +74,16 @@ class Ejercicio extends LitElement {
     handleKeyDown(event) {
         if (event.key === "Enter" && this.arrayPersonajes.length > 0) {
             const value = event.target.value;
-            // this.arrayFiltrados = this.arrayPersonajes.filter(personaje => personaje.name == value);
             this.arrayFiltrados = this.arrayPersonajes.filter(personaje => personaje.name.toLowerCase().includes(value));
+            this.valueInput = value;
             event.target.value = '';
+
         }
     }
 
     selectPersonaje(event) {
         const selectedHouse = event.target.value;
+        this.value = selectedHouse;
         if (selectedHouse && this.arrayPersonajes.length > 0) {
             this.arraySelectFilter = this.arrayPersonajes.filter(personaje =>
                 personaje.house.toLowerCase().includes(selectedHouse.toLowerCase())
@@ -48,41 +96,49 @@ class Ejercicio extends LitElement {
     render() {
         return html`
 
-            <h1>Buscar Personaje:</h1>
-            <input type="text" @keydown=${this.handleKeyDown}>
-            ${this.arrayPersonajes
+
+            <section class="section">
+                <h2>Buscar Personaje:</h2>
+                <div class="input-container">
+                    <input class="input" type="text" @keydown=${this.handleKeyDown}>
+                </div>
+
+                ${this.valueInput
                 ? html`
-                    <component-list .arrayPersonajes="${this.arrayFiltrados}" ></component-list>
-                `
-                : `Cargando Personajes`
-            }
+                        <h3>Resultados para: ${this.valueInput}</h3>
+                        <component-list .arrayPersonajes="${this.arrayFiltrados}" ></component-list>
+                    `
+                : ``}
+            </section>
+
+            <section class="section">
+
+                <h2>Seleccionar Personaje</h2>
+                    <div class="select-container">
+                        <select class="select-style" name="personajes" @change="${this.selectPersonaje}">
+                            <option value="Selecciona un valor">Selecciona un valor</option>
+                            ${this.arraySelect.map(house => html` <option value="${house}">${house}</option>`)}
+                        </select>
+                    </div>
 
 
-            <h1>Seleccionar Personaje</h1>
-            <select name="personajes" @change="${this.selectPersonaje}">
-                <option value="Selecciona un valor">Selecciona un valor</option>
-                ${this.arraySelect.map(house => html`
-                    <option value="${house}">${house}</option>
-                `)}
-            </select>
-
-            ${this.arraySelectFilter.length > 0
+                
+                ${this.arraySelectFilter.length > 0
                 ? html`
+                    <h3>Resultados para ${this.value}</h3>
                     <component-list .arrayPersonajes="${this.arraySelectFilter}" ></component-list>
-                `
-                : ``
-            }
+                    `
+                : ``}
+            </section>
             
-
-        
-            <h1>Todos los personajes: </h1>
-            
-            ${this.arrayPersonajes
+            <section class="section">
+                <h2>Todos los personajes: </h2>
+                ${this.arrayPersonajes
                 ? html`
                     <component-list .arrayPersonajes="${this.arrayPersonajes}" ></component-list>
-                `
-                : `Cargando Personajes`
-            }
+                    `
+                : ``}
+            </section>
 
 
 
